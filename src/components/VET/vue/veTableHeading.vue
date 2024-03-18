6ec2b6cf86d7c70b313ee7b2b52644222b63e77d
unknown <ataota.org@gmail.com>
2024-03-19 00:55:23 +0530

    Version-0.4.9

--- src/components/VET/vue/veTableHeading.vue
+++ src/components/VET/vue/veTableHeading.vue
@@ -1,29 +1,29 @@
 <script setup>
 let props = defineProps({
-    HeaderItems: Object,
-    tableGridStyle: String,
-})
+  HeaderItems: Object,
+  tableGridStyle: String,
+});
 </script>
 
 <template>
-    <div class="veTableWrapper" 
-    :style="{'grid-template-columns': props.tableGridStyle}">
-        <div v-for="item in HeaderItems" :key="item.id" class="veTableContent">
-            {{ item }}
-        </div>
+  <div class="veTableWrapper" :style="{ 'grid-template-columns': props.tableGridStyle }">
+    <div v-for="item in HeaderItems" :key="item.id" class="veTableContent">
+      {{ item }}
     </div>
+  </div>
 </template>
 
 <style scoped>
-.veTableWrapper{
-    display: grid;
-    text-align: left;
+.veTableWrapper {
+  display: grid;
+  text-align: left;
+  color: black;
 }
-.veTableContent{
-    box-sizing: border-box;
-    border: 1px solid blue;
-    padding: 5px;
-    display: flex;
-    align-items: center;
+.veTableContent {
+  box-sizing: border-box;
+  border: 1px solid blue;
+  padding: 5px;
+  display: flex;
+  align-items: center;
 }
 </style>
