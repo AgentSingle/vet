49bd15a6c637e99ccb385dafce354e688f30e8fb
unknown <ataota.org@gmail.com>
2024-03-19 22:43:59 +0530

    VERSION-0.6.3

--- src/components/VET/vue/vet.vue
+++ src/components/VET/vue/vet.vue
@@ -3,11 +3,13 @@ import { ref, onMounted, defineEmits } from "vue";
 const emit = defineEmits(["tableDataChangeResponse"]);
 import veTableHeading from "./veTableHeading.vue";
 import vetInput from "./vetInput.vue";
+import IconDelete from "../../VET/icon/IconDelete.vue";
 import { modifyObjectById, addOrReplaceObject } from "../js/algorithm.js";
 let tableGridStyle = ref("100px 1fr 1fr 1fr 1fr");
 let tableMinWidth = ref("800px");
 let vetTableHeaderItems = ref([]);
 let chckBoxWidth = ref(40);
+let defaultActionWidth = ref(40);
 
 let props = defineProps({
   VetHederItems: Object,
@@ -17,17 +19,18 @@ let props = defineProps({
 
 onMounted(() => {
   let cbWidth = chckBoxWidth.value;
+  let daWidth = defaultActionWidth.value;
 
   if (props.vetDefaultItems) {
     let dD = props.vetDefaultItems; // default data
 
     // MINIMUM WIDHT-SETUP OF THE SCROLLABLE TABLE
     let sumMinDimensions = dD.reduce((acc, obj) => acc + obj.minDimension, 0);
-    tableMinWidth.value = `${cbWidth + sumMinDimensions}px`;
+    tableMinWidth.value = `${cbWidth + daWidth + sumMinDimensions}px`;
 
     // GIVING DIMENSION TO EACH INDIVIDUAL TABLE ROW
     let gridStyle = dD.map((value) => `${value.dimension}`).join(" ");
-    tableGridStyle.value = `${cbWidth}px ${gridStyle}`;
+    tableGridStyle.value = `${cbWidth}px ${daWidth}px ${gridStyle}`;
     console.warn(tableGridStyle.value);
 
     // CREATE HEADER TABLE NAME
@@ -97,6 +100,16 @@ const setChangeValue = async (e) => {
             <input type="checkbox" name="myCheckbox" value="yes" />
           </div>
 
+          <!-- STYLING INDIVIDUAL DELETE ACTION BUTTON -->
+          <div
+            class="vetTableContent"
+            style="display: flex; align-items: center; justify-content: center"
+          >
+            <button>
+              <IconDelete :dimension="20" iconColor="red"></IconDelete>
+            </button>
+          </div>
+
           <!-- TABLE INDIVIDUAL CELL -->
           <div
             v-for="(item, index) in vetTableHeaderItems"
@@ -163,5 +176,23 @@ const setChangeValue = async (e) => {
   border: 1px solid blue;
   box-sizing: border-box;
   overflow: hidden;
+}
+</style>
+
+<style scoped>
+button {
+  height: 30px;
+  width: auto;
+  display: grid;
+  align-content: center;
+  background-color: transparent;
+  border: none;
+  outline: 0;
+}
+button :focus {
+  border: none;
+  outline: 0;
+  -webkit-user-select: none;
+  user-select: none;
 }
 </style>
